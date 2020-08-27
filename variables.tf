2a3e5bb5c312b4216351a45649a952e500b79966
Mark Menger <51541068+mjmenger@users.noreply.github.com>
2020-08-27 20:44:31 +0000

    initial AKS resources

--- variables.tf
+++ variables.tf
@@ -1,1 +1,19 @@
 # variables
+variable "resourceName" {
+  default = "AKS-NGINX-TF-DEMO"
+}
+variable "clusterName" {
+  default = "kubernetes-aks1"
+}
+variable "location" {
+  default = "northeurope"
+}
+variable "dnsPrefix" {
+  default = "kubecluster"
+}
+variable "instanceSize" {
+  default = "Standard_D2_v2"
+}
+variable "agentNodes" {
+  default = "1"
+}
